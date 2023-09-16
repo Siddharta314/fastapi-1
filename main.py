98e53ed262997491e3e39e74ec3376a8d99036f5
Emilio Sala <emilio_k88@hotmail.com>
2023-09-16 22:41:09 +0200

    a few changes to the docs of swagger

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+# Making changes to the docs
+app.title = "first application fastAPI"
+app.version = "0.0.1"
 
 
-@app.get("/")
+"""
+Another Way
+app = FastAPI(
+    title= 'first application fastAPI',
+    description= 'Una API solo por diversión',
+    version= '0.0.1',
+)
+"""
+
+
+@app.get("/", tags=["home"])
 def message():
     return "Hello World"
+
+
+# to run terminal: uvicorn main:app
+# option --reload --port 5000
