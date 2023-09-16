943f8f70e2a15d8bea2bdec5a7372d40488dada8
Emilio Sala <emilio_k88@hotmail.com>
2023-09-16 23:11:30 +0200

    get_cat from id and raise http exception if not found

--- cats.py
+++ cats.py
@@ -1,1 +1,5 @@
-cats = [{"name": "rodrigo", "age": 36}, {"name": "pepe", "age": 20}]
+cats = [
+    {"id": 1, "name": "rodrigo", "age": 36},
+    {"id": 2, "name": "pepe", "age": 20},
+    {"id": 3, "name": "Alexandra", "age": 1},
+]
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import HTMLResponse
 from cats import cats
 
@@ -27,6 +27,14 @@ def message():
 @app.get("/cats", tags=["cats"])
 def get_cats():
     return cats
+
+
+@app.get("/cats/{id}", tags=["cats"])
+def get_cat(id: int):
+    for cat in cats:
+        if cat["id"] == id:
+            return cat
+    raise HTTPException(status_code=404, detail="Cat missing")
 
 
 # to run terminal: uvicorn main:app
